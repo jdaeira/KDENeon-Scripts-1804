7082c567980c77ac0e973d9560c4349190d823ff
jdaeira <jdaeira@gmail.com>
2018-09-26 13:51:38 -0700

    add initial scripts and files

---
+++ Install-Scripts/100-apps-to-install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+set -e
+########################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+########################################################################
+
+
+sudo apt install build-essential -y
+sudo apt install aptitude -y
+sudo apt install nfs-common -y
+sudo apt install tilix -y
+sudo apt install neofetch -y
+sudo apt install synaptic -y
+
+echo "############ Common Apps Installed #############"
+
---
+++ Install-Scripts/105-kde-apps.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+set -e
+########################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+########################################################################
+
+
+sudo apt install dolphin-plugins -y
+sudo apt install kdegraphics -y
+sudo apt install kdegraphics-thumbnailers -y
+sudo apt install ffmpegthumbs -y
+sudo apt install kate -y
+sudo apt install kaccounts-integration -y
+sudo apt install kaccounts-providers -y
+sudo apt install kcolorchooser -y
+
+
+echo "############ Common Apps Installed #############" 
