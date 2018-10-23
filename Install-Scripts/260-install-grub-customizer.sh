408f5188cd6af2c085f4a6769a194f3aae72d187
John da Eira <jdaeira@gmail.com>
2018-10-22 18:17:39 -0700

    add a install grub customizer script

---
+++ Install-Scripts/260-install-grub-customizer.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -e
+########################################################################
+# Author    : John da Eira
+# Email     : jdaeira@gmail.com
+########################################################################
+
+
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt update
+sudo sudo apt install grub-customizer -y
+
+echo "###########         Install Grub Customizer          #############"
