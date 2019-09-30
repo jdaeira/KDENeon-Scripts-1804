ce990f27ac1dbff31f8933ae8d38e1738029d49b
jdaeira <jdaeira@gmail.com>
2019-09-30 16:00:21 -0700

    added libinput-gestures.conf file

--- Install-Scripts/105-kde-apps.sh
+++ Install-Scripts/105-kde-apps.sh
@@ -11,15 +11,15 @@ sudo apt install kdegraphics -y
 sudo apt install kdegraphics-thumbnailers -y
 sudo apt install ffmpegthumbs -y
 sudo apt install kate -y
-sudo apt install kaccounts-integration -y
-sudo apt install kaccounts-providers -y
+#sudo apt install kaccounts-integration -y
+#sudo apt install kaccounts-providers -y
 sudo apt install kcolorchooser -y
 sudo apt install krita -y
 sudo apt install kcalc -y
 sudo apt install muon -y
 sudo apt install kfind -y
 sudo apt install ttf-mscorefonts-installer -y
-sudo apt install kubuntu-driver-manager -y
+#sudo apt install kubuntu-driver-manager -y
 
 
 echo "############ Common Apps Installed #############" 
