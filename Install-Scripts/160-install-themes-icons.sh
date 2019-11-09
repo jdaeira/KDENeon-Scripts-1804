99ff6a6247bc463bec1fc57412f1bfda3d3271e7
jdaeira <jdaeira@gmail.com>
2019-11-09 14:43:56 -0800

    added an icon and the shade of red in konsole.txt

--- Install-Scripts/160-install-themes-icons.sh
+++ Install-Scripts/160-install-themes-icons.sh
@@ -6,19 +6,19 @@ set -e
 ########################################################################
 
 
-sudo add-apt-repository ppa:tista/adapta -y
-sudo apt update
-sudo apt install adapta-backgrounds -y
-sudo apt install adapta-gtk-theme -y
-echo "######## Adapta Theme Installed ##########"
+#sudo add-apt-repository ppa:tista/adapta -y
+#sudo apt update
+#sudo apt install adapta-backgrounds -y
+#sudo apt install adapta-gtk-theme -y
+#echo "######## Adapta Theme Installed ##########"
 
-sudo apt install arc-theme -y
-echo "########## ARC Theme Installed ###########"
+#sudo apt install arc-theme -y
+#echo "########## ARC Theme Installed ###########"
 
-git clone https://github.com/erikdubois/Surfn
-sudo cp Surfn/Surf* -r /usr/share/icons/
-sudo rm -r Surfn
-echo "########## Surfn Icons Installed ###########"
+#git clone https://github.com/erikdubois/Surfn
+#sudo cp Surfn/Surfn* -r /usr/share/icons/
+#sudo rm -r Surfn
+#echo "########## Surfn Icons Installed ###########"
 
 
 git clone https://github.com/erikdubois/Sardi-Extra
@@ -26,8 +26,8 @@ sudo cp Sardi-Extra/Sardi* -r /usr/share/icons/
 sudo rm -r Sardi-Extra
 echo "########## Sardi Icons Installed ###########"
 
-sudo apt install papirus-icon-theme -y
-echo "########## Papirus Icons Installed ##########"
+#sudo apt install papirus-icon-theme -y
+#echo "########## Papirus Icons Installed ##########"
 
-sudo apt install materia-gtk-theme -y
-echo "########## Materia Theme Installed ##########"
+#sudo apt install materia-gtk-theme -y
+#echo "########## Materia Theme Installed ##########"
